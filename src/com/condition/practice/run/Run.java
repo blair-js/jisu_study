32ab569e7f1f535ba8aa46b26f3b62dcabfee9f8
blair-js <jih-su@hanmail.net>
2022-01-12 00:08:59 +0900

    0112_조건문 공부

---
+++ src/com/condition/practice/run/Run.java
@@ -0,0 +1,23 @@
+package com.condition.practice.run;
+
+import com.condition.practice.example.ConditionPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		ConditionPractice c = new ConditionPractice();
+		//c.practice1();
+		//c.practice2();
+		//c.practice3();
+		//c.practice4();
+		//c.practice5();
+		//c.practice6();
+		//c.practice7();
+		//c.practice8();
+		//c.practice9();
+		c.practice10();
+		
+	}
+
+}
