7b46f791e9f1fd8060942058bbbb354335d5e1ee
blair-js <jih-su@hanmail.net>
2022-01-11 12:17:23 +0900

    0111_연산자공부

---
+++ src/com/operator/practice/run/Run.java
@@ -0,0 +1,26 @@
+package com.operator.practice.run;
+
+import com.operator.practice.example.OperatorPractice;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		OperatorPractice op = new OperatorPractice();
+		//op.practice1();
+		//op.practice3();
+		//op.practice4();
+		//op.practice5();
+		//op.practice6();
+		//op.practice7();
+		//op.practice8();
+		//op.practice9();
+		//op.practice10();
+		op.practice11();
+		
+		//retest 
+		
+		
+	}
+
+}
