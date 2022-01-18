62c899f4117e5e9ab43dfa37e3a8c2514ae1c906
blair-js <jih-su@hanmail.net>
2022-01-18 23:28:46 +0900

    0119_이차원배열 공부(4번, 8번, 9번 다시풀기)

---
+++ src/com/array2/practice/run/Run.java
@@ -0,0 +1,22 @@
+package com.array2.practice.run;
+
+import com.array2.practice.example.Array2Practice;
+
+public class Run {
+
+	public static void main(String[] args) {
+
+		Array2Practice ap = new Array2Practice();
+		//ap.practice1();
+		//ap.practice2();
+		//ap.practice3();
+		//ap.practice4();
+		//ap.practice5();
+		//ap.practice6();
+		//ap.practice7();
+		ap.practice8();
+		
+		
+	}
+
+}
