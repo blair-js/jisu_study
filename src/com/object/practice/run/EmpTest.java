6e7e85ad5faa320b5e1921140eb7cc8ea79954c4
blair-js <jih-su@hanmail.net>
2022-01-19 23:46:57 +0900

    0119_클래스 공부_2번,3번 문제까지 완료

---
+++ src/com/object/practice/run/EmpTest.java
@@ -0,0 +1,13 @@
+package com.object.practice.run;
+
+import com.object.practice.view.EmpMenu;
+
+public class EmpTest {
+
+	public static void main(String[] args) {
+
+		EmpMenu e = new EmpMenu();
+		e.mainMenu();
+	}
+
+}
